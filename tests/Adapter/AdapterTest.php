c7f97e90a6ae4a5b7a15965c37111ceac653c5dc
lucaszz <lucaszz111@gmail.com>
2015-10-10 17:26:19 +0200

    Project setup

---
+++ tests/Adapter/AdapterTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Lucaszz\FacebookAuthenticationAdapter\Tests\Adapter;
+
+class AdapterTest extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function test_it()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
